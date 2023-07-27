1daea9d5a2d0db2706bb500487d02c7173ef6b73
Eduardo Macedo <eduardomacedo.eng@hotmail.com>
2023-07-27 17:18:53 -0300

    Adding security auth in project

--- mini-autorizador/src/main/java/br/com/cadmus/miniautorizador/controller/UserController.java
+++ mini-autorizador/src/main/java/br/com/cadmus/miniautorizador/controller/UserController.java
@@ -4,6 +4,7 @@ import br.com.cadmus.miniautorizador.dto.UserDTO;
 import br.com.cadmus.miniautorizador.service.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.ResourceAccessException;
 
@@ -17,6 +18,8 @@ public class UserController {
         this.userServiceImpl = userServiceImpl;
     }
 
+    //TODO: This method can be accessed by user whose role is admin
+    @PreAuthorize("hasRole('client_cadmus_admin')")
     @GetMapping("/all")
     public ResponseEntity<?> findAllUsers() {
         try {
@@ -26,16 +29,20 @@ public class UserController {
         }
     }
 
+    //TODO: This method can be accessed by user whose role is user
+    @PreAuthorize("hasAnyRole('client_cadmus_user', 'client_cadmus_admin')")
     @GetMapping("/byID/{id}")
     public ResponseEntity<UserDTO> findUserByID(@PathVariable Long id) {
         return ResponseEntity.ok(userServiceImpl.findByID(id));
     }
 
+    @PreAuthorize("hasRole('client_cadmus_admin')")
     @PutMapping("/update/{id}")
     public ResponseEntity<UserDTO> findUserByID(@PathVariable Long id, @RequestBody UserDTO usuarioDTO) {
         return ResponseEntity.ok(userServiceImpl.updateUser(id, usuarioDTO));
     }
 
+    @PreAuthorize("hasRole('client_cadmus_admin')")
     @PostMapping("/new")
     public ResponseEntity<UserDTO> newUser(@RequestBody UserDTO userDTO) {
         return new ResponseEntity<>(userServiceImpl.saveNewUser(userDTO), HttpStatus.CREATED);
